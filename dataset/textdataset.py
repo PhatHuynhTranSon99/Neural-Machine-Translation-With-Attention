a21b3e711583faa6b75e7a0b83c45576c7bb8968
phat <phat.huynhtranso99@gmail.com>
2021-11-18 16:13:16 +0700

    Added custom collate function for data loader

--- dataset/textdataset.py
+++ dataset/textdataset.py
@@ -1,5 +1,34 @@
+from typing import *
 import torch
+import numpy as np
 from language.corpus import DualingualCorpus
+
+
+def collate_fn(batch: List[Tuple[List[int], List[int], int]]):
+    """
+    Preprocess the batch so that source_sentences are sorted by length descending
+    Params: batch is a list containing batch_size number of samples
+
+    Returns: Tuple(source_sentences, target_sentences, source_length)
+    source_sentences (List[List[int]]): list of tokenize source sentences
+    target_sentences (List[List[int]]): list of tokenize target sentences
+    source_length (List[int]): list of lengths of source sentences
+    """
+    # Extract the source_sentences, target_sentences and source_lengths
+    source_sentences = [item[0] for item in batch]
+    target_sentences = [item[1] for item in batch]
+    source_lengths = [item[2] for item in batch]
+
+    # Sort the index of source_lengths in descending order
+    indices_sorted_by_length = list(np.argsort(source_lengths))[::-1]
+
+    # Sort the source sentences, target sentences and source lengths based on the indices
+    source_sentences = [source_sentences[index] for index in indices_sorted_by_length]
+    target_sentences = [target_sentences[index] for index in indices_sorted_by_length]
+    source_lengths = [source_lengths[index] for index in indices_sorted_by_length]
+
+    return source_sentences, target_sentences, source_lengths
+
 
 """
 Create a custom dualingual dataset
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import torch
 from language.corpus import DualingualCorpus, map_sentences_inverse
+from dataset.textdataset import CustomDualingualDataset, collate_fn
 
 SOURCE_TRAIN = "data/train.vi"
 TARGET_TRAIN = "data/train.en"
@@ -19,3 +21,22 @@ if __name__ == "__main__":
         source_vocab=train_source_vocab,
         target_vocab=train_target_vocab
     )
+
+    train_dataset = CustomDualingualDataset(train_corpus)
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        shuffle=True,
+        batch_size=64,
+        collate_fn=collate_fn
+    )
+
+    source_sentences, target_sentences, source_length = next(iter(train_dataloader))
+
+    print(len(source_sentences))
+    print(len(target_sentences))
+    print(source_length)
+
+    print(map_sentences_inverse(source_sentences[:1], train_source_vocab))
+    print(map_sentences_inverse(target_sentences[:1], train_target_vocab))
+
+    print(len(source_sentences[0]))
